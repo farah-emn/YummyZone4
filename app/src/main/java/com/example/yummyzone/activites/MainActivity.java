0066a3d79c9bdfca63930375b330f84046cbfd25
batool400 <layan2000222@gmail.com>
2023-06-30 18:09:08 +0300

    b6

--- app/src/main/java/com/example/yummyzone/activites/MainActivity.java
+++ app/src/main/java/com/example/yummyzone/activites/MainActivity.java
@@ -1,4 +1,5 @@
-     package com.example.yummyzone.activites;
+package com.example.yummyzone.activites;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -14,29 +15,39 @@ import com.example.yummyzone.fragment.profileFragment;
 import com.example.yummyzone.fragment.searchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-     public class MainActivity extends AppCompatActivity {
-         BottomNavigationView nav;
+
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent;
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView nav;
+    String post_key1 = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
         nav = findViewById(R.id.main_nav);
         String postkey = "";
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment,new homeFragment(postkey)).commit();
+        String name = "";
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, new homeFragment(post_key1)).commit();
         nav.setSelectedItemId(R.id.home);
         nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment = null;
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.home:
-                        fragment = new homeFragment(postkey);
+                        fragment = new homeFragment(post_key1);
+
                         break;
 
                     case R.id.search:
                         fragment = new searchFragment();
+
                         break;
 
                     case R.id.favorite:
@@ -45,24 +56,18 @@ import com.google.android.material.navigation.NavigationBarView;
                         break;
 
                     case R.id.cart:
-                        fragment = new cartFragment();
+                        fragment = new cartFragment(name);
                         break;
 
                     case R.id.settings:
                         fragment = new profileFragment();
                         break;
                 }
-                 getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, fragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, fragment).commit();
+
                 return true;
             }
         });
 
-    }    @Override
-         public void onBackPressed() {
-             if (getFragmentManager().getBackStackEntryCount() > 0) {
-                 getFragmentManager().popBackStack();
-             } else {
-                 super.onBackPressed();
-             }
-         }
+    }
 }
--- app/src/main/java/com/example/yummyzone/adapter/restaurantAdapter.java
+++ app/src/main/java/com/example/yummyzone/adapter/restaurantAdapter.java
@@ -1,33 +1,21 @@
 package com.example.yummyzone.adapter;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.yummyzone.R;
-import com.example.yummyzone.activites.MainActivity;
-import com.example.yummyzone.activites.MenuActivity;
-import com.example.yummyzone.classes.Menu_tab;
 import com.example.yummyzone.classes.restaurant;
-import com.example.yummyzone.fragment.Menu;
-import com.example.yummyzone.fragment.favoriteFragment;
+import com.example.yummyzone.fragment.MenuFragment;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-
-import java.util.ArrayList;
-
 
 public class restaurantAdapter extends FirebaseRecyclerAdapter<restaurant,restaurantAdapter.myviewholder>
 {
@@ -38,24 +26,20 @@ public class restaurantAdapter extends FirebaseRecyclerAdapter<restaurant,restau
     protected void onBindViewHolder(@NonNull myviewholder holder, int position, @NonNull final restaurant model) {
         holder.name.setText(model.getName());
         Glide.with(holder.image.getContext()).load(model.getImage()).into(holder.image);
-        Glide.with(holder.image.getContext()).load(model.getImage()).into(holder.image);
         final String  post_key = getRef(position).getKey();
         //holder.pre.setText(model.getPre());
         //holder.price.setText(model.getPrice());
       //  Glide.with(holder.img1.getContext()).load(model.getImage()).into(holder.img1);
         holder.image.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {String post_key1 = getRef(position).getKey();
-                //   AppCompatActivity activity=(AppCompatActivity)view.getContext();
-            //    Fragment fragment = new homeFragment(post_key);
-             //    Intent f=new Intent(view.getContext(), MenuActivity.class);
-               // f.putExtra("cate",post_key);
-                //view.getContext().startActivity(f);
-            Menu fragment=new Menu(post_key1);
-                ((FragmentActivity)view.getContext()).getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.main_fragment, fragment)
-                        .commit();
+            public void onClick(View view) {
+                MenuFragment fragment=new MenuFragment();
+                Bundle bundle=new Bundle();
+                bundle.putString("cate",post_key);
+                fragment.setArguments(bundle);
+                AppCompatActivity activity=(AppCompatActivity)view.getContext();
+                activity.getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment,fragment).commit();
+
             }
         });
 
@@ -70,13 +54,13 @@ public class restaurantAdapter extends FirebaseRecyclerAdapter<restaurant,restau
 
     public class myviewholder extends RecyclerView.ViewHolder
     {
-        ImageView img1;
+        ImageView image;
         TextView name,pre,calories,price;
-                ImageView image;
+
 
         public myviewholder(@NonNull View itemView) {
             super(itemView);
-//img1=itemView.findViewById(R.id.)
+
             image=itemView.findViewById(R.id.homeScreen_restaurant_image);
             name=itemView.findViewById(R.id.homeScreen_tv_restaurantName);
 
