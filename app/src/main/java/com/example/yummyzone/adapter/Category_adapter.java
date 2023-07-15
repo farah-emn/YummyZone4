e6b01524d4a0f9da45992d84177ec5815ad86872
batool400 <layan2000222@gmail.com>
2023-07-15 05:09:12 +0300

    b8

--- app/src/main/java/com/example/yummyzone/adapter/Category_adapter.java
+++ app/src/main/java/com/example/yummyzone/adapter/Category_adapter.java
@@ -1,19 +1,14 @@
 package com.example.yummyzone.adapter;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
 import com.example.yummyzone.classes.Category_tab;
 import com.example.yummyzone.R;
